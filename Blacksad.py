21faea5a96e5625441fcadfb09498f13a39434ea
Youenn Denis <github@youenndenis.fr>
2015-05-30 16:36:20 +0200

    sdqsd

--- Blacksad.py
+++ Blacksad.py
@@ -6,9 +6,3 @@ software_version = '0.21.0'
 print "Blacksad project v_%s" % software_version
 
 # (c) Youenn Denis - MAY 2015
-
-import smbus
-import sys
-import getopt
-import time
-bus = smbus.SMBus(1)
---
+++ Switchs.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+import sys
+import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(22,GPIO.IN)
+GPIO.setup(23,GPIO.IN)
+
+
+while True:
+  if (GPIO.input(22)):
+    print("Button 1 Pressed")
+
+while True:
+      if (GPIO.input(23)):
+        print("Button 2 Pressed")
