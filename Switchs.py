b23e96f82708d55363d1d223b5ee789c88baa72d
Youenn Denis <github@youenndenis.fr>
2015-05-30 18:07:41 +0200

    test cleaup

--- Switchs.py
+++ Switchs.py
@@ -2,27 +2,20 @@
 
 import sys
 import time
-
-#Avoiding crashing
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print 'System Interrupted'
-        try:
-            sys.exit(0)
-            GPIO.cleanup()
-        except SystemExit:
-            os._exit(0)
-
-            import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) #Using BCM pinout
 input_list = [22,23] #GPIO inputs for the switches
 output_list = [6,13,19,26,12,16,20,21] #GPIO outputs for the steppers and in the future the laser led
 GPIO.setup(input_list, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(output_list, GPIO.OUT, initial=GPIO.LOW)
-while True:
-    if (GPIO.input(22)):
-        print("Button 1 Pressed");
-    if (GPIO.input(23)):
-        print("Button 2 Pressed");
+try:
+
+    while True:
+        if (GPIO.input(22)):
+            print("Button 1 Pressed");
+        if (GPIO.input(23)):
+            print("Button 2 Pressed");
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
+        print "GoodBye"
