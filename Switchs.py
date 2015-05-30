6487e7c4a1dd21d5a05a3fb4dcc4d2b439f66e5e
Youenn Denis <github@youenndenis.fr>
2015-05-30 17:49:21 +0200

    test exit

--- Switchs.py
+++ Switchs.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print 'Interrupted'
+        print 'System Interrupted'
         try:
             sys.exit(0)
             GPIO.cleanup()
@@ -19,11 +19,10 @@ if __name__ == '__main__':
 GPIO.setmode(GPIO.BCM) #Using BCM pinout
 input_list = [22,23] #GPIO inputs for the switches
 output_list = [6,13,19,26,12,16,20,21] #GPIO outputs for the steppers and in the future the laser led
-GPIO.setup(input_list,GPIO.IN pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(output_list,GPIO.OUT, initial=GPIO.LOW)
-
+GPIO.setup(input_list, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(output_list, GPIO.OUT, initial=GPIO.LOW)
+while True:
     if (GPIO.input(22)):
         print("Button 1 Pressed");
-
     if (GPIO.input(23)):
         print("Button 2 Pressed");
