8c8b3bea96b8de04a7499c3f1afcbdbf59d9645d
Youenn Denis <github@youenndenis.fr>
2015-05-30 18:42:07 +0200

    marchepo

--- Switchs.py
+++ Switchs.py
@@ -8,23 +8,16 @@ input_list = [22,23] #GPIO inputs for the switches
 output_list = [6,13,19,26,12,16,20,21] #GPIO outputs for the steppers and in the future the laser led
 GPIO.setup(input_list, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(output_list, GPIO.OUT, initial=GPIO.LOW)
-GPIO.add_event_detect(22, GPIO.BOTH)
-GPIO.add_event_detect(23, GPIO.BOTH)
-GPIO.add_event_callback(22, GPIO.RISING, sw01_rising)
-GPIO.add_event_callback(22, GPIO.FALLING, sw01_falling)
-GPIO.add_event_callback(23, GPIO.RISING, sw02_rising)
-GPIO.add_event_callback(23, GPIO.FALLING, sw02_falling)
+GPIO.add_event_detect(22, GPIO.RISING, callback=my_callback)
 try:
 
     while True:
-        def sw01_rising(channel):
-            print('Switch %s Rising'%channel)
-        def sw01_falling(channel):
-            print('Switch %s Falling'%channel)
-        def sw01_rising(channel):
-            print('Switch %s Rising'%channel)
-        def sw01_falling(channel):
-            print('Switch %s Falling'%channel)
+        def my_callback(channel):
+    print('This is a edge event callback function!')
+    print('Edge detected on channel %s'%channel)
+    print('This is run in a different thread to your main program')
+
+
 
 
 
