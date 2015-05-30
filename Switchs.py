efbfc4bffd801576c135824b1a16833037924d71
Youenn Denis <github@youenndenis.fr>
2015-05-30 17:39:43 +0200

    test crash

--- Switchs.py
+++ Switchs.py
@@ -2,15 +2,26 @@
 
 import sys
 import time
-import RPi.GPIO as GPIO
 
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
+#Avoiding crashing
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        try:
+            sys.exit(0)
+            GPIO.cleanup()
+        except SystemExit:
+            os._exit(0)
 
-GPIO.setup(22,GPIO.IN)
-GPIO.setup(23,GPIO.IN)
-while True:
+            import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM) #Using BCM pinout
+input_list = [22,23] #GPIO inputs for the switches
+output_list = [6,13,19,26,12,16,20,21] #GPIO outputs for the steppers and in the future the laser led
+GPIO.setup(input_list,GPIO.IN pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(output_list,GPIO.OUT, initial=GPIO.LOW)
+
     if (GPIO.input(22)):
         print("Button 1 Pressed");
 
